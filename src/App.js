a99b1cb5e9a4d8f895ecba13f710796feffb6cba
dboya <94754886+dboya@users.noreply.github.com>
2023-01-22 09:35:25 -0500

    using react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          welcome Dave /Unbeatable to Github
         </p>
         <a
           className="App-link"
